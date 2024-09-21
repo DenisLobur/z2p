67400c72d98c47d86e2a599e21b67f1562b2255d
DenysLobur <denismachtalles@gmail.com>
2024-09-21 21:46:53 +0300

    Add tracing

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn main() -> Result<(), std::io::Error> {
     // `init` does call `set_logger`, so this is all we need to do.
     // We are falling back to printing all logs at info-level or above
     // if the RUST_LOG environment variable has not been set.
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("trace")).init();
 
     // Panic if we can't read the configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -16,15 +16,27 @@ pub async fn subscribe(
 ) -> HttpResponse {
     // Let's generate a random unique identifier
     let request_id = Uuid::new_v4();
-    // We are using the same interpolation syntax of `println`/`print` here!
-    log::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
-
+    // Spans, like logs, have an associated level
+    // `info_span` creates a span at the info-level
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        %request_id,
+        email = %form.email,
+        name = %form.name
     );
-    log::info!(
+    // Using `enter` in an async function is a recipe for disaster!
+    // Bear with me for now, but don't do this at home.
+    // See the following section on `Instrumenting Futures`
+    let _request_span_guard = request_span.enter();
+
+    // We are using the same interpolation syntax of `println`/`print` here!
+    // tracing::info!(
+    //     "request_id {} - Adding '{}' '{}' as a new subscriber.",
+    //     request_id,
+    //     form.email,
+    //     form.name
+    // );
+    tracing::info!(
         "request_id {} - Saving new subscriber details in the database",
         request_id
     );
@@ -43,7 +55,7 @@ pub async fn subscribe(
         .await
     {
         Ok(_) => {
-            log::info!(
+            tracing::info!(
                 "request_id {} - New subscriber details have been saved",
                 request_id
             );
@@ -53,7 +65,7 @@ pub async fn subscribe(
             // Using `println!` to capture information about the error
             // in case things don't work out as expected
             // println!("Failed to execute query: {:?}", e);
-            log::error!(
+            tracing::error!(
                 "request_id {} - Failed to execute query: {:?}",
                 request_id,
                 e
